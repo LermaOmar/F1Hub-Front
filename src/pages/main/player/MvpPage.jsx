c517ddfbbb7b478181f39bebbeab976e71688ab7
LermaOmar <omarlerma2004@gmail.com>
2025-05-27 23:11:51 +0200

    Creación de la página de mvps

---
+++ src/pages/main/player/MvpPage.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import MVPCard from '../../../components/MvpCard';
+import '../../../styles/Mvp.css';
+import axiosInstance from '../../../utils/AxiosInstance';
+import { useNavigate } from 'react-router-dom';
+
+const MVPPage = () => {
+  const [teamMVP, setTeamMVP] = useState(null);
+  const [driverMVPs, setDriverMVPs] = useState([]);
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+
+    axiosInstance.get('/teams/mvp')
+      .then(response => {
+        const id = response.data.id;
+        return axiosInstance.get(`/teams/${id}`);
+      })
+      .then(response => setTeamMVP(response.data))
+      .catch(error => console.error('❌ Error fetching full team MVP:', error));
+
+    axiosInstance.get('/drivers/mvps')
+      .then(response => {
+        const mvps = response.data.content || [];
+        return Promise.all(
+          mvps.map(driver => axiosInstance.get(`/drivers/${driver.id}`).then(res => res.data))
+        );
+      })
+      .then(fullDrivers => setDriverMVPs(fullDrivers))
+      .catch(error => console.error('❌ Error fetching full driver MVPs:', error));
+  }, []);
+
+
+  useEffect(()=> {console.log(teamMVP)},[teamMVP])
+
+  return (
+    <>
+    
+        <h1 className="mvp-title">GP MVP's</h1>
+        <div className="header-controls">
+            <button className="back-btn" onClick={() => navigate('/dashboard')}>
+                ← Back to Dashboard
+            </button>
+        </div>
+
+        <section className="mvp-section">
+            <h2 className="mvp-subtitle">Top Performers</h2>
+            <div className="mvp-cards">
+                {teamMVP && (
+                <MVPCard
+                    name={teamMVP.name}
+                    image={teamMVP.imageUrl}
+                    stats={{
+                    Nationality: teamMVP.nationality,
+                    Points: teamMVP.previousPoints,
+                    Price: `$${teamMVP.price.toLocaleString()}`
+                    }}
+                />
+                )}
+
+                {driverMVPs.map(driver => (
+                <MVPCard
+                    key={driver.id}
+                    name={driver.name}
+                    image={driver.imageUrl}
+                    stats={{
+                    Nationality: driver.nationality,
+                    Points: driver.previousPoints,
+                    Price: `$${driver.price.toLocaleString()}`
+                    }}
+                />
+                ))}
+            </div>
+        </section>
+    </>
+
+  );
+};
+
+export default MVPPage;
